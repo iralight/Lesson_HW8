e60f128c4b1570a8b114e726e2ff925ed1aeac13
Irina Likhtina <iralight@gmail.com>
2014-04-20 23:25:24 -0400

    now the thing that is broken is the user is not persisted

--- HW8/app/controllers/users/omniauth_callbacks_controller.rb
+++ HW8/app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 def twitter
     auth = env["omniauth.auth"]
 
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
     if @user.persisted?
       puts "user persisted"
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
--- HW8/app/models/user.rb
+++ HW8/app/models/user.rb
@@ -2,37 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :omniauthable, omniauth_providers: [:twitter]
          
 def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(:email => auth.uid + "@twitter.com").first
-      if registered_user
-        return registered_user
-      else
-        puts auth.inspect
-        #user = User.create(name:auth.extra.raw_info.name,
-        #                    provider:auth.provider,
-        #                    uid:auth.uid,
-        #                    email:auth.uid+"@twitter.com",
-        #                    password:Devise.friendly_token[0,20],
-        #                  )
-        user = User.create(provider:auth.provider,
-                           uid:auth.uid,
-                           email:auth.uid+"@twitter.com",
-                           encrypted_password:Devise.friendly_token[0,20])
-      end
+  puts 'inside find_for_twitter_oath'
+  puts auth.slice(:provider, :uid)
 
-    end
-  end     
-
-  private
-
-  def user_params
-  	params.require(:provider).permit(:provider, :uid)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider,
+      user.uid = auth.uid,
+      user.email = auth.uid+"@twitter.com",
+      user.encrypted_password = Devise.friendly_token[0,20]
   end
+end     
 
 end
